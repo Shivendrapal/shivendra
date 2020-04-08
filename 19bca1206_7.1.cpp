a73795ee2509df6e3ff57c8a1fe23a69673083b9
Shivendrapal <63361715+Shivendrapal@users.noreply.github.com>
2020-04-08 21:30:09 +0530

    Create 19bca1206_7.1.cpp

---
+++ 19bca1206_7.1.cpp
@@ -0,0 +1,43 @@
+
+
+
+
+#include<iostream>
+
+
+using namespace std;
+
+
+int main()
+{
+int a,b;
+cout<<"enter two number "; cin>>a>>b;
+
+ 
+try
+{
+
+
+
+
+
+
+}
+ 
+
+
+
+if(b==0)
+
+
+throw b; cout<<"result="<<a/b;
+ 
+
+
+catch(int m)
+{
+cout<<"input number is not valid = "<<m;
+}
+ 
+
+
